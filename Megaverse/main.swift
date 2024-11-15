68f6f17f643dbb2a97c2d15b4f86b8ba10d2eb22
Ander Goig <4272387+AnderGoig@users.noreply.github.com>
2024-11-15 16:03:20 +0100

    Phase 1 code

--- Megaverse/main.swift
+++ Megaverse/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+do {
+    print("👋 Welcome to Megaverse!")
 
+    let candidateId = "8d87846f-31de-48cf-90e9-82a24475d201"
+    let networkService = MegaverseNetworkService(candidateId: candidateId)
+
+    print("⏳ Fetching current goal...")
+    let goal = try await networkService.request(MegaverseAPI.goal(for: candidateId)).goal
+    print("✅ Goal fetched")
+
+    for row in goal.enumerated() {
+        for column in row.element.enumerated() {
+            switch column.element {
+            case "SPACE":
+                break
+            case "POLYANET":
+                print("🪐 Creating \(column.element) at (\(row.offset),\(column.offset))...")
+                try await networkService.request(MegaverseAPI.Polyanets.create(atRow: row.offset, column: column.offset))
+            default:
+                print("⁉️ Unknown object \"\(column.element)\" at (\(row.offset),\(column.offset))")
+            }
+        }
+    }
+} catch {
+    print("🔴 ERROR:", error.localizedDescription)
+}
